7acb915cf18d2b6401ea7bb05917cdb9d1b78910
ridoyislamnasim <ridoyislamnasim@gmail.com>
2023-10-20 23:50:09 +0600

    add form

--- src/components/form.js
+++ src/components/form.js
@@ -1,20 +1,39 @@
-export default function Form() { 
+import { useForm } from 'react-hook-form'
+import { DevTool } from '@hookform/devtools'
+export default function Form() {
+    const { register, handleSubmit, formState, control } = useForm();
+    const { errors } = formState;
+    const onSubmit = (data) => {
+        console.log('onsumit form ', data)
+    }
     return (
-        <form>
-            <label>User Name</label>
-            <input type="text" id="userName" name="username"/>
+        <>
+            <form onSubmit={handleSubmit(onSubmit)} >
+                <label>User Name</label>
+                <input type="text" id="userName" {...register("username", {
+                    required: 'Username is required'
+                })} />
+                <p>{errors.username?.message}</p>
+                <br /><br />
 
-            <br/><br/>
+                <label>User Email</label>
+                <input type="text" id="userEmail" {...register("useremail", {
+                    required: 'userEmail is required',
+                    pattern: {
+                        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                        message: 'Invalid email formate'
+                    }
+                })} />
+                <p>{errors.useremail?.message}</p>
 
-            <label>User Name</label>
-            <input type="text" id="userName" name="username"/>
-            
-            <br/><br/>
+                <br /><br />
 
-            <label>User Name</label>
-            <input type="text" id="userName" name="username"/>
-            <br/><br/>
-            <button>Submit</button>
-        </form>
+                <label>User Name</label>
+                <input type="text" id="userName" name="username" />
+                <br /><br />
+                <button>Submit</button>
+            </form>
+            <DevTool control={control} />
+        </>
     )
 }
